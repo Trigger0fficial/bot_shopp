6b5255d97bba4d26ea2d04a71570de2da4893144
Trigger06 <nikitin_dima2000dn@mail.ru>
2021-09-22 02:44:20 +0300

    Bot is work!

--- data/config.py
+++ data/config.py
@@ -3,7 +3,7 @@ from environs import Env
 # Теперь используем вместо библиотеки python-dotenv библиотеку environs
 env = Env()
 env.read_env()
-# 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
+
 BOT_TOKEN = env.str("BOT_TOKEN")  # Забираем значение типа str
 ADMINS = env.list("ADMINS_1")  # Тут у нас будет список из админов
 IP = env.str("ip")  # Тоже str, но для айпи адреса хоста
